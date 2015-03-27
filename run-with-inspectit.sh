24ea5a77df5990d25ede3117fd684b3ff0459cc6
Andreas Reinhardt <andreas.reinhardt@novatec-gmbh.de>
2015-03-27 08:30:51 +0100

    Add message if configuration for CMR is missing

--- run-with-inspectit.sh
+++ run-with-inspectit.sh
@@ -6,6 +6,12 @@ else
 	cp -r $INSPECTIT_CONFIG_HOME/../config/* $INSPECTIT_CONFIG_HOME
 	CMR_ADDR=${INSPECTIT_CMR_ADDR:-$CMR_PORT_9070_TCP_ADDR}
 	CMR_PORT=${INSPECTIT_CMR_PORT:-$CMR_PORT_9070_TCP_PORT}
+
+	if [ -z $CMR_ADDR ] || [ -z $CMR_PORT ]; then
+                echo "No inspectIT CMR configured! Please read our README"
+                exit 1
+        fi
+
 	AGENT_NAME=${AGENT_NAME:-$HOSTNAME}
 	sed -i "s/^\(repository\) .*/\1 $CMR_ADDR $CMR_PORT $AGENT_NAME/" $INSPECTIT_CONFIG_HOME/inspectit-agent.cfg
 	echo "Done. Remember to modify the configuration for your needs. You find the configuration in the mapped volume $INSPECTIT_CONFIG_HOME." 
