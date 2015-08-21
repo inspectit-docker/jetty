035b97d52916eec7318c8bd5f7eb5c2e47fcd706
Andreas Reinhardt <andreas.reinhardt@novatec-gmbh.de>
2015-08-21 09:56:53 +0200

    Add version checks before starting the server

--- run-with-inspectit.sh
+++ run-with-inspectit.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 if [ "$(ls -A $INSPECTIT_CONFIG_HOME)" ]; then
-	echo "Using existing inspectIT configuration..."
+	echo "[INFO] Using existing inspectIT configuration..."
 else
 	if ([ -z $INSPECTIT_CMR_ADDR ] || [ -z $INSPECTIT_CMR_PORT ]) && ([ -z $CMR_PORT_9070_TCP_ADDR ] || [ -z $CMR_PORT_9070_TCP_PORT ]); then
-                echo "No inspectIT CMR configured! Please read our README"
+                echo "[ERROR] No inspectIT CMR configured! Please read our README"
                 exit 1
         fi
 
-	echo "No custom inspectIT configuration found, using default one..."
+	echo "[INFO] No custom inspectIT configuration found, using default one..."
 	cp -r $INSPECTIT_CONFIG_HOME/../config/* $INSPECTIT_CONFIG_HOME
 	CMR_ADDR=${INSPECTIT_CMR_ADDR:-cmr}
 	CMR_PORT=${INSPECTIT_CMR_PORT:-9070}
 
 	AGENT_NAME=${AGENT_NAME:-$HOSTNAME}
 	sed -i "s/^\(repository\) .*/\1 $CMR_ADDR $CMR_PORT $AGENT_NAME/" $INSPECTIT_CONFIG_HOME/inspectit-agent.cfg
-	echo "Done. Remember to modify the configuration for your needs. You find the configuration in the mapped volume $INSPECTIT_CONFIG_HOME." 
+	echo "[INFO] Done. Remember to modify the configuration for your needs. You find the configuration in the mapped volume $INSPECTIT_CONFIG_HOME." 
+fi
+
+# Version check
+if ([[ -n $INSPECTIT_VERSION && -n $CMR_ENV_INSPECTIT_VERSION && $INSPECTIT_VERSION != $CMR_ENV_INSPECTIT_VERSION ]]); then
+	echo "[ERROR] Version of inspectIT CMR and agent do not match:"
+	echo "[ERROR] Agent: $INSPECTIT_VERSION"
+	echo "[ERROR] CMR: $CMR_ENV_INSPECTIT_VERSION"
+	echo "[ERROR] You have to use the same version for CMR and agent."
+	exit 1
+else
+	CMR_REST_VERSION=$(wget http://$CMR_ADDR:$CMR_PORT/rest/cmr/version -qO-)
+	if ([[ $? -eq 0 && $INSPECTIT_VERSION != $CMR_REST_VERSION ]]); then
+		echo "[ERROR] Version of inspectIT CMR and agent do not match:"
+		echo "[ERROR] Agent: $INSPECTIT_VERSION"
+		echo "[ERROR] CMR: $CMR_REST_VERSION"
+		echo "[ERROR] You have to use the same version for CMR and agent."
+		exit 1
+	fi
+	echo "[INFO] Running inspectIT with version $INSPECTIT_VERSION"
+	echo "[INFO] Make sure your CMR has the same version"
 fi
 
 exec jetty.sh run
